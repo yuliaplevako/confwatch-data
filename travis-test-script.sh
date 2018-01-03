b83696bcc543e94bc009f6c7ac1b7aace1bbcf4f
evilsocket <evilsocket@gmail.com>
2018-01-03 21:56:22 +0100

    test: travis build file fixes

--- travis-test-script.sh
+++ travis-test-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -rf "$GOPATH/src/github.com/ConfWatch/confwatchd" &&
 go get "github.com/ConfWatch/confwatchd" && 
 cd "$GOPATH/src/github.com/ConfWatch/confwatchd" &&
 make && 
+cd -- &&
 "$GOPATH/src/github.com/ConfWatch/confwatchd/confwatchd" -config "$GOPATH/src/github.com/ConfWatch/confwatchd/dev-config.json" -seed .
